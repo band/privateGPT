9cfd09d5b66083284df17731ecf9825fb6cb2cbd
William L. Anderson <band@acm.org>
2023-05-28 15:44:23 -0500

    python copyediting

--- privateGPT.py
+++ privateGPT.py
@@ -1,13 +1,15 @@
-#!/usr/bin/env python3
-from dotenv import load_dotenv
+#! /usr/bin/env python3
+
+import argparse
+import os
+
 from langchain.chains import RetrievalQA
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.vectorstores import Chroma
 from langchain.llms import GPT4All, LlamaCpp
-import os
-import argparse
 
+from dotenv import load_dotenv
 load_dotenv()
 
 embeddings_model_name = os.environ.get("EMBEDDINGS_MODEL_NAME")
@@ -21,8 +23,18 @@ target_source_chunks = int(os.environ.get('TARGET_SOURCE_CHUNKS',4))
 from constants import CHROMA_SETTINGS
 
 def main():
-    # Parse the command line arguments
+    # Parse command line arguments
+    def parse_arguments():
+        parser = argparse.ArgumentParser(description='privateGPT: Ask questions to your documents without an internet connection, '
+                                                 'using the power of LLMs.')
+        parser.add_argument("--hide-source", "-S", action='store_true',
+                        help='Use this flag to disable printing of source documents used for answers.')
+        parser.add_argument("--mute-stream", "-M",
+                        action='store_true',
+                        help='Use this flag to disable the streaming StdOut callback for LLMs.')
+        return parser.parse_args()
     args = parse_arguments()
+
     embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
     db = Chroma(persist_directory=persist_directory, embedding_function=embeddings, client_settings=CHROMA_SETTINGS)
     retriever = db.as_retriever(search_kwargs={"k": target_source_chunks})
@@ -37,41 +49,28 @@ def main():
         case _default:
             print(f"Model {model_type} not supported!")
             exit;
+
     qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents= not args.hide_source)
     # Interactive questions and answers
     while True:
         query = input("\nEnter a query: ")
-        if query == "exit":
-            break
+        match query.split():
+            case ["exit" | "quit"]:
+                quit()
+            case _:
+            # Get the answer from the chain
+                res = qa(query)
+                answer, docs = res['result'], [] if args.hide_source else res['source_documents']
+                # Print the result
+                print(f"\n\n> Question: {query!r}")
+                print("\n> Answer:")
+                from pprint import pprint
+                pprint(answer)
 
-        # Get the answer from the chain
-        res = qa(query)
-        answer, docs = res['result'], [] if args.hide_source else res['source_documents']
-
-        # Print the result
-        print("\n\n> Question:")
-        print(query)
-        print("\n> Answer:")
-        from pprint import pprint
-        pprint(answer)
-
-        # Print the relevant sources used for the answer
-        for document in docs:
-            print("\n> " + document.metadata["source"] + ":")
-#            print(document.page_content)
-
-def parse_arguments():
-    parser = argparse.ArgumentParser(description='privateGPT: Ask questions to your documents without an internet connection, '
-                                                 'using the power of LLMs.')
-    parser.add_argument("--hide-source", "-S", action='store_true',
-                        help='Use this flag to disable printing of source documents used for answers.')
-
-    parser.add_argument("--mute-stream", "-M",
-                        action='store_true',
-                        help='Use this flag to disable the streaming StdOut callback for LLMs.')
-
-    return parser.parse_args()
-
+                # Print the relevant sources used for the answer
+                for document in docs:
+                    print("\n> " + document.metadata["source"] + ":")
+#                   print(document.page_content)
 
 if __name__ == "__main__":
     main()
