f5cdca185bba05e2fe9fc3c06f19c7b500179e3f
William L. Anderson <band@acm.org>
2023-06-04 11:52:50 -0500

    rewrite CL arg parsing

--- privateGPT.py
+++ privateGPT.py
@@ -1,20 +1,28 @@
 #! /usr/bin/env python3
 
-import argparse
-import os
-
 from langchain.chains import RetrievalQA
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.vectorstores import Chroma
 from langchain.llms import GPT4All, LlamaCpp
 
+# set up argparse
+import argparse
+def init_argparse():
+    parser = argparse.ArgumentParser(description='privateGPT: Use LLMS to ask questions of your documents with no internet connection')
+    parser.add_argument("--hide-source", "-S", action='store_true',
+                        help='Use this flag to disable printing source documents used for answers.')
+    parser.add_argument("--mute-stream", "-M",
+                        action='store_true',
+                        help='Use this flag to disable the streaming StdOut callback for LLMs.')
+    return parser
+
 from dotenv import load_dotenv
+import os
 load_dotenv()
 
 embeddings_model_name = os.environ.get("EMBEDDINGS_MODEL_NAME")
 persist_directory = os.environ.get('PERSIST_DIRECTORY')
-
 model_type = os.environ.get('MODEL_TYPE')
 model_path = os.environ.get('MODEL_PATH')
 model_n_ctx = os.environ.get('MODEL_N_CTX')
@@ -23,17 +31,8 @@ target_source_chunks = int(os.environ.get('TARGET_SOURCE_CHUNKS',4))
 from constants import CHROMA_SETTINGS
 
 def main():
-    # Parse command line arguments
-    def parse_arguments():
-        parser = argparse.ArgumentParser(description='privateGPT: Ask questions to your documents without an internet connection, '
-                                                 'using the power of LLMs.')
-        parser.add_argument("--hide-source", "-S", action='store_true',
-                        help='Use this flag to disable printing of source documents used for answers.')
-        parser.add_argument("--mute-stream", "-M",
-                        action='store_true',
-                        help='Use this flag to disable the streaming StdOut callback for LLMs.')
-        return parser.parse_args()
-    args = parse_arguments()
+    argparser = init_argparse()
+    args = argparser.parse_args()
 
     embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
     db = Chroma(persist_directory=persist_directory, embedding_function=embeddings, client_settings=CHROMA_SETTINGS)
